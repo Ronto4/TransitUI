d67d9cf749ee829da38a143f6643b5b52ab1fb05
Ronto4 <34981840+Ronto4@users.noreply.github.com>
2021-10-30 11:06:16 +0200

    Add missing bin bash

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 docker kill transit-web-container
 docker rm transit-web-container
 docker build -f TransitWeb/Dockerfile -t transit-web . && docker run -p 3654:80 -d --name transit-web-container transit-web
