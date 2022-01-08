810dd3d53d476664766533e62f65376b2f528b94
Ronto4 <34981840+Ronto4@users.noreply.github.com>
2022-01-08 20:04:49 +0100

    Add set -e

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+set -e
+
 ./startup
 docker run -p 3654:80 --name transit-web-container transit-web
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 docker kill transit-web-container
 docker rm transit-web-container
 docker build -f TransitWeb/Dockerfile -t transit-web .
