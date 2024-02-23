9d6faad752e6c8df11d9f96f8d84da186cd88ba9
Ronto4 <34981840+Ronto4@users.noreply.github.com>
2024-02-24 00:06:02 +0100

    Improve change text

--- Timetables/Models/IHistoryEntry.cs
+++ Timetables/Models/IHistoryEntry.cs
@@ -5,5 +5,9 @@ public interface IHistoryEntry
     public DateOnly EffectiveDate { get; }
     internal IReadOnlyDictionary<string, Line> LinesById { get; }
     public IEnumerable<KeyValuePair<string, Line>> OrderedLinesById => LinesById.OrderBy(entry => entry.Key);
+    /// <summary>
+    /// A short text, to be rendered above the line overview.
+    /// </summary>
+    /// <remarks>This field supports HTML rendering.</remarks>
     public string Description { get; }
 }
