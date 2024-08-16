a8efdf92798bce4c3e68ce6e12a38bb3b9a28cac
Ronto4 <34981840+Ronto4@users.noreply.github.com>
2024-08-16 18:51:19 +0200

    Add 96

--- Timetables/Vip/Lines/Tram96/Tram96.cs
+++ Timetables/Vip/Lines/Tram96/Tram96.cs
@@ -3,5 +3,5 @@ namespace Timetables.Vip.Lines.Tram96;
 internal class Tram96 : ICompleteLine
 {
     public IEnumerable<ILineInstance> LineInstances { get; } =
-        [new Tram96From20240102(), new Tram96From20240606(), new Tram96From20240608(), new Tram96From20240610()];
+        [new Tram96From20240102(), new Tram96From20240606(), new Tram96From20240608(), new Tram96From20240610(), new Tram96From20240816()];
 }
