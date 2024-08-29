c91a5dd38c272dd059641ef3ce0a7ea17581bfad
Ronto4 <34981840+Ronto4@users.noreply.github.com>
2024-08-29 19:55:55 +0200

    Add 93

--- Timetables/Vip/Lines/Tram93/Tram93.cs
+++ Timetables/Vip/Lines/Tram93/Tram93.cs
@@ -2,5 +2,6 @@ namespace Timetables.Vip.Lines.Tram93;
 
 internal class Tram93 : ICompleteLine
 {
-    public IEnumerable<ILineInstance> LineInstances { get; } = [new Tram93From20240102()];
+    public IEnumerable<ILineInstance> LineInstances { get; } =
+        [new Tram93From20240102(), new Tram93From20240826Until20240831()];
 }
