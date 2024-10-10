bbe4f195d115193b5e2c0e9439fa3a1fc34802ef
Ronto4 <34981840+Ronto4@users.noreply.github.com>
2024-10-10 19:58:18 +0200

    Fix history overview text

---
+++ Timetables/Vip/Lines/Tram92/Tram92From20240923.cs
@@ -0,0 +1,6 @@
+namespace Timetables.Vip.Lines.Tram92;
+
+public class Tram92From20240923
+{
+    
+}
---
+++ Timetables/Vip/Lines/Tram99/Tram99From20240921Until20240922.cs
@@ -0,0 +1,6 @@
+namespace Timetables.Vip.Lines.Tram99;
+
+public class Tram99From20240921Until20240922
+{
+    
+}
