2a963538f369dfdcbb1b6cecb10462c08522ce30
Ronto4 <34981840+Ronto4@users.noreply.github.com>
2024-10-10 19:59:10 +0200

    Fix 92 after construction weekend

--- Timetables/Vip/Lines/Tram92/Tram92.cs
+++ Timetables/Vip/Lines/Tram92/Tram92.cs
@@ -6,5 +6,6 @@ internal class Tram92 : ICompleteLine
     [
         new Tram92From20240102(), new Tram92From20240422(), new Tram92From20240606(), new Tram92From20240608(),
         new Tram92From20240610(), new Tram92From20240816Until20240818(), new Tram92From20240921Until20240922(),
+        new Tram92From20240923(),
     ];
 }
