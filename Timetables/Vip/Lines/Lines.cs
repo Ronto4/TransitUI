85f16d9431cd442a338803d23004f10a5d869b68
Ronto4 <34981840+Ronto4@users.noreply.github.com>
2025-01-29 01:42:53 +0100

    Add 695

--- Timetables/Vip/Lines/Lines.cs
+++ Timetables/Vip/Lines/Lines.cs
@@ -16,6 +16,7 @@ internal static class Lines
         ["bus692"] = new Bus692.Bus692(),
         ["bus693"] = new Bus693.Bus693(),
         ["bus694"] = new Bus694.Bus694(),
+        ["bus695"] = new Bus695.Bus695(),
         ["tram91"] = new Tram91.Tram91(),
         ["tram92"] = new Tram92.Tram92(),
         ["tram93"] = new Tram93.Tram93(),
