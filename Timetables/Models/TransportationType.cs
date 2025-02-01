2d638027d82974ee778febc0590cbe6968d1d7cf
Ronto4 <34981840+Ronto4@users.noreply.github.com>
2025-02-01 01:54:13 +0100

    Add natural customer ordering for lines

--- Timetables/Models/TransportationType.cs
+++ Timetables/Models/TransportationType.cs
@@ -1,13 +1,14 @@
 // ReSharper disable InconsistentNaming
 namespace Timetables.Models;
 
+// The exact numeric values are used for natural sorting of lines. Change with care!
 public enum TransportationType
 {
-    LongDistanceTrains,
-    RegionalTrains,
-    SBahn,
-    UBahn,
-    Tram,
-    Bus,
-    Ferry,
+    LongDistanceTrains = 10,
+    RegionalTrains = 20,
+    SBahn = 30,
+    UBahn = 40,
+    Tram = 50,
+    Bus = 60,
+    Ferry = 70,
 }
