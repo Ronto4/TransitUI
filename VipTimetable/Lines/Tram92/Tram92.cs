5ea828960600b37949244965ec04dd550f5e8ce3
Ronto4 <34981840+Ronto4@users.noreply.github.com>
2025-08-29 17:55:37 +0200

    data: add 92 & fix small error in 92

--- VipTimetable/Lines/Tram92/Tram92.cs
+++ VipTimetable/Lines/Tram92/Tram92.cs
@@ -7,6 +7,6 @@ internal class Tram92 : ICompleteLine
         new Tram92From20240102(), new Tram92From20240422(), new Tram92From20240606(), new Tram92From20240608(),
         new Tram92From20240610(), new Tram92From20240816Until20240818(), new Tram92From20240921Until20240922(),
         new Tram92From20240923(), new Tram92From20241012Until20241013(), new Tram92From20241104(),
-        new Tram92From20241215(), new Tram92From20250110Until20250112(),
+        new Tram92From20241215(), new Tram92From20250110Until20250112(), new Tram92From20250203(),
     ];
 }
