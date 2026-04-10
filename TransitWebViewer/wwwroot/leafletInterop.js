a23e7994594b6f85a9c2897d1847d94448cdf478
Ronto4 <34981840+Ronto4@users.noreply.github.com>
2026-04-10 23:58:51 +0200

    feat: improve transit map
    * Use line colours for markers.
    * Auto-refresh every ten seconds.
    * Show trip paths when clicking a vehicle.
    * Improve display of via and continuing notes in trip headsign.
    
    Known issues:
    * When a trip or popup text (= headsign) changes, the update is not properly propagated to the map.
    * The heading of a vehicle is not included.

--- TransitWebViewer/wwwroot/leafletInterop.js
+++ TransitWebViewer/wwwroot/leafletInterop.js
@@ -1,3 +1,11 @@
+const vehicles = {};
+const routes = {};
+const removeAllRoutes = () => {
+    for (const [tripId, polyline] of Object.entries(routes)) {
+        polyline.remove();
+        delete routes[tripId];
+    }
+};
 window.leafletMap = {
     initialize: function (mapId, lat, lng, zoom) {
         // Initialize the Leaflet map
@@ -12,8 +20,86 @@ window.leafletMap = {
         return map;
     },
 
-    vehicleAt: function (map, lat, lng, popupText) {
-        return L.marker([lat, lng]).addTo(map)
+    vehicleAt: function (dotNetObject, map, vehicleId, lat, lng, popupText, colour, tripId) {
+        if (vehicles[vehicleId]) {
+            const [marker, oldTripId] = vehicles[vehicleId];
+            marker.setLatLng([lat, lng]);
+            marker.color = colour;
+            marker.setTooltipContent(popupText);
+            if (marker.isPopupOpen() && marker.getPopup().content !== popupText) {
+                console.log(`Resetting popup old '${marker.getPopup().content}' new '${popupText}'.`);
+                marker.closePopup();
+                marker.openPopup();
+            }
+            if (oldTripId !== tripId) {
+                console.log(`Resetting trip ${tripId} (was ${oldTripId}) (${popupText}).`);
+                if (routes[oldTripId]) {
+                    console.log("Redrawing!");
+                    removeAllRoutes();
+                    dotNetObject.invokeMethodAsync('OnOpenTripMarker', tripId);
+                }
+                vehicles[vehicleId] = [marker, tripId];
+            }
+            return;
+        }
+        const marker = L.circleMarker([lat, lng], { color: colour });
+        vehicles[vehicleId] = [marker, tripId];
+        return marker
+            .addTo(map)
+            .on('popupopen', _event => {
+                if (routes[tripId]) {
+                    return;
+                }
+                removeAllRoutes();
+                dotNetObject.invokeMethodAsync('OnOpenTripMarker', tripId);
+            })
+            // .on('popupclose', _event => {
+            //     if (routes[tripId]) {
+            //         routes[tripId].remove();
+            //         delete routes[tripId];
+            //     } else {
+            //         console.warn(`Could not remove path of trip ${tripId}.`);
+            //     }
+            // })
             .bindPopup(popupText);
+        // return L.marker([lat, lng]).addTo(map)
+           //  .bindPopup(popupText);
+    },
+
+    keepOnlyVehicles: function (vehicleIds, a) {
+        for (const [vehicleId, [marker, tripId]] of Object.entries(vehicles)) {
+          if (vehicleIds.includes(vehicleId)) {
+              continue;
+          }
+          marker.remove();
+          if (routes[tripId]) {
+              routes[tripId].remove();
+              delete routes[tripId];
+          }
+          delete vehicles[vehicleId];
+        }
+    },
+
+    drawRoute: function (map, points, colour, focus, tripId) {
+        const leafletPoints = points.map(point => new L.LatLng(point.latitude, point.longitude));
+        const polyline = new L.polyline(leafletPoints, {
+            color: colour,
+            weight: 3,
+            opacity: 1,
+            smoothFactor: 1,
+        });
+        if (tripId) {
+            if (routes[tripId]) {
+                routes[tripId].remove();
+            }
+            routes[tripId] = polyline;
+        } else {
+            console.warn(`Could not store polyline for colour ${colour}.`);
+        }
+        if (focus) {
+            map.fitBounds(polyline.getBounds());
+        }
+        polyline.addTo(map);
+        return polyline.bringToBack();
     }
 };
