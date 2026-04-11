debf43d05099a557ad34beb07fc24145bae8c355
Ronto4 <34981840+Ronto4@users.noreply.github.com>
2026-04-11 18:18:56 +0200

    data: add 695

--- VipTimetable/Lines/Bus695/Bus695.cs
+++ VipTimetable/Lines/Bus695/Bus695.cs
@@ -2,5 +2,5 @@ namespace VipTimetable.Lines.Bus695;
 
 internal class Bus695 : ICompleteLine
 {
-    public IEnumerable<ILineInstance> LineInstances { get; } = [new Bus695From20241214()];
+    public IEnumerable<ILineInstance> LineInstances { get; } = [new Bus695From20241214(), new Bus695From20250203()];
 }
