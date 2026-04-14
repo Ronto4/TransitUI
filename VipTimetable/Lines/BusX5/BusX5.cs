80dea0d93a459993eb3383eebd9133ba9c22d3b8
Ronto4 <34981840+Ronto4@users.noreply.github.com>
2026-04-14 16:23:28 +0200

    data: add X5 not operating from 2026-02-09 to 2026-04-10

--- VipTimetable/Lines/BusX5/BusX5.cs
+++ VipTimetable/Lines/BusX5/BusX5.cs
@@ -6,6 +6,6 @@ internal class BusX5 : ICompleteLine
     [
         new BusX5From20241214Until20241220(), new BusX5From20250106(), new BusX5From20250203(),
         new BusX5From20250210Until20250404(), new BusX5On20250502(), new BusX5On20250530(),
-        new BusX5From20250721Until20251010(),
+        new BusX5From20250721Until20251010(), new BusX5From20260209Until20260410(),
     ];
 }
