3f64d0242988b88f8c13a497e2f285b78af7adef
Ronto4 <34981840+Ronto4@users.noreply.github.com>
2026-04-16 00:50:07 +0200

    --wip-- [skip ci]

--- VipTimetable/Lines/ILineInstance.cs
+++ VipTimetable/Lines/ILineInstance.cs
@@ -1,13 +1,28 @@
 using Timetable;
 
-namespace VipTimetable.Lines;
-
-internal interface ILineInstance
+namespace VipTimetable.Lines
 {
-    public DateOnly ValidFrom { get; }
 
-    public DateOnly? ValidUntilInclusive() => ValidityMode() is Timetable.ValidityMode.OnlyOnThisDay ? ValidFrom : null;
+    internal interface ILineInstance
+    {
+        public DateOnly ValidFrom { get; }
 
-    public ValidityMode ValidityMode() => Timetable.ValidityMode.Regular;
-    public Line Line { get; }
+        public DateOnly? ValidUntilInclusive() =>
+            ValidityMode() is Timetable.ValidityMode.OnlyOnThisDay ? ValidFrom : null;
+
+        public ValidityMode ValidityMode() => Timetable.ValidityMode.Regular;
+        public Line Line { get; }
+    }
+    public union Test(int, string);
+}
+
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false)]
+    public sealed class UnionAttribute : Attribute;
+
+    public interface IUnion
+    {
+        object? Value { get; }
+    }
 }
